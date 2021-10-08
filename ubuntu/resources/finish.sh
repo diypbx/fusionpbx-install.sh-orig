330dd5e15e13669d6d10b1654631bfbe87e723b8
diypbx <68690323+diypbx@users.noreply.github.com>
2021-10-08 12:32:00 +0100

    Update finish.sh
    Removed Unrequired info

--- ubuntu/resources/finish.sh
+++ ubuntu/resources/finish.sh
@@ -114,23 +114,6 @@ echo "   The domain name in the browser is used by default as part of the authen
 echo "   If you need to login to a different domain then use username@domain."
 echo "      username: $user_name@$domain_name";
 echo ""
-echo "   Official FusionPBX Training"
-echo "      Fastest way to learn FusionPBX. For more information https://www.fusionpbx.com."
-echo "      Available online and in person. Includes documentation and recording."
-echo ""
-echo "      Location:               Online"
-echo "      Admin Training:         TBA"
-echo "      Advanced Training:      TBA"
-echo "      Continuing Education:   https://www.fusionpbx.com/training"
-echo "      Timezone:               https://www.timeanddate.com/weather/usa/idaho"
-echo ""
-echo "   Additional information."
-echo "      https://fusionpbx.com/members.php"
-echo "      https://fusionpbx.com/training.php"
-echo "      https://fusionpbx.com/support.php"
-echo "      https://www.fusionpbx.com"
-echo "      http://docs.fusionpbx.com"
-echo ""
 
 
 
